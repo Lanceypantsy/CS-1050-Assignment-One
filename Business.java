12e617764c6455685212200848815aed365d2f00
Lanceypantsy <34278955+Lanceypantsy@users.noreply.github.com>
2018-02-10 16:41:32 -0700

    Add files via upload

---
+++ Business.java
@@ -0,0 +1,19 @@
+
+/**
+ * Project Assignment: Introduce a Business. List four (4) special promotions
+ * by the business.
+ *
+ * @aLance Barto
+ * @v1.0
+ */
+public class Business
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Barto's World of Fine Food!!");
+        System.out.println("\nJoin us every Monday for Happy Hour pricing all night!");
+        System.out.println("\n\"Wine down Wednesdays\" offer half off all wine bottles!");
+        System.out.println("\nWeekend reservations are filling up, Call us to reserve today!");
+        System.out.println("\nOn July 7th, we host winemaker and actor Kurt Russel for a wine dinner in our cellar. Ask your server for details.");
+    }    
+}
